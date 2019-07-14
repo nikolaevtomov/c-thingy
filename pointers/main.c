820e4a444c5cb83ab97f9c55ab5f1553b921ee8b
Nikolaev Tomov <nikolaev.github@yahoo.com>
2019-07-14 16:14:36 +0100

    Pass by referance

--- pointers/main.c
+++ pointers/main.c
@@ -1,5 +1,8 @@
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void square(int * x);
 
 int main() {
   int num = 150;
@@ -10,7 +13,18 @@ int main() {
   printf("Address of num: %p\n", &num);
   printf("Value of pNum: %p\n", pNum);
   printf("Address of pNum: %p\n", &pNum);
-  printf("Value at pNum pointing to: %d\n", *pNum);
+  printf("Value at pNum pointing to: %d\n\n", *pNum);
+
+  // dynamic memory allocation
+  int *someNumber = (int *)malloc(sizeof(int));
+  *someNumber = 4;
+  // pass by reference
+  square(someNumber);
+  printf("The value of someNumber: %d\n\n", *someNumber);
 
   return 0;
+}
+
+void square(int * x) {
+  *x = (*x) * (*x);
 }
